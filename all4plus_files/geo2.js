aa3fdf5a4c31d897cae1a282990f69858cc46f58
James Nash <jmnxvii@gmail.com>
2021-08-02 14:38:00 +0100

    Add files via upload

---
+++ all4plus_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "WIRRAL",
+      'continent': "EU",
+      'country': "GB",
+      'region': "EN"
+    },
+    'ip':"2.27.169.16"
+  }]);
+})
+//
+()
+
+;
